b2f8aed3b1d569e7ae7908fea7f993c068e362cd
HotNuggets <mishamandark@gmail.com>
2025-04-30 15:22:45 +0300

    schema

--- server/src/models/bet/betModel.ts
+++ server/src/models/bet/betModel.ts
@@ -1,9 +1,20 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 export const BetSchema = new mongoose.Schema({
     betDescription:String,
-    options:String[],
-    
+    createdBy:{
+        type: Schema.Types.ObjectId,
+        ref: 'User', 
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+      expiresAt: {
+        type: Date,
+      },
 
 
 })
+export const Bet = mongoose.model('Bet',BetSchema);
