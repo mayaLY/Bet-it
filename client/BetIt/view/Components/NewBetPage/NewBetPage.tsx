6178596f0dfd43a58f81f8b073d16f31638d1846
HotNuggets <mishamandark@gmail.com>
2025-05-31 16:21:33 +0300

    a

--- client/BetIt/view/Components/NewBetPage/NewBetPage.tsx
+++ client/BetIt/view/Components/NewBetPage/NewBetPage.tsx
@@ -35,7 +35,7 @@ export default function SurveyBuilderPage() {
     };
 
     
-   // navigation.navigate('SurveyDisplay', { survey });
+   /// navigation.navigate('SurveyDisplay', { survey });
   };
 
   return (
