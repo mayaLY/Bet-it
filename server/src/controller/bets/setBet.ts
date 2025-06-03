5bb1d649f95e5690ba72ddc6aea3b5c1d2c9dfd5
HotNuggets <mishamandark@gmail.com>
2025-06-03 19:55:03 +0300

    tests

--- server/src/controller/bets/setBet.ts
+++ server/src/controller/bets/setBet.ts
@@ -2,8 +2,12 @@ import { Bet } from "../../models/bet/betModel";
 
 export async function setBet(req: any, res: any) {
     try {
+        console.log("got here");
+         console.log(req.body);
       const  {betDescription,expiresAt,options} =req.body;
-      console.log(betDescription,expiresAt,options);
+       //const createdBy = req.user.userId;
+       console.log(req.user.userId);
+      console.log(betDescription,expiresAt,options,"what");
       const bet = new Bet({betDescription,expiresAt,options});
       await bet.save();
     } catch (error) {
