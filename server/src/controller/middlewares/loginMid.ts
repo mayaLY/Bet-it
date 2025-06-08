288d2a23b29a5ffa093288e7d466e7e3789d7ff8
MayaLY <maya_levy@yahoo.com>
2025-06-08 18:57:01 +0300

    middleware function test

--- server/src/controller/middlewares/loginMid.ts
+++ server/src/controller/middlewares/loginMid.ts
@@ -1,28 +1,39 @@
-import { NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 import { User } from "../../models/user/userModel";
+import jwt from "jsonwebtoken";
 
-export async function checkUser(req: any, res: any, next: NextFunction) {
-    try {
-        // Do something
-        const { userId } = req.cookies;
-        console.log(userId);
-
-        const userDB = await User.findById(userId);
-
-        if (!userDB) {
-            res.status(401).send({ error: 'User not found' });
-            return;
-        } 
-
-        req.user = userDB;
-
-        next();
-
-        
-       
-    } catch (error) {
-        console.error(error);
-        res.send(error);
-
+// Extend Express Request interface to include 'user'
+declare global {
+  namespace Express {
+    interface Request {
+      user?: typeof User.prototype;
     }
+  }
+}
+
+export async function checkUser(req: Request, res: Response, next: NextFunction) {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).send({ error: "Missing or invalid token" });
+    }
+
+    const token = authHeader.split(" ")[1];
+    const secret = process.env.JWT_SECRET as string;
+
+    const decoded = jwt.verify(token, secret) as { userId: string };
+
+    const userDB = await User.findById(decoded.userId);
+
+    if (!userDB) {
+      return res.status(401).send({ error: "User not found" });
+    }
+
+    req.user = userDB;
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(401).send({ error: "Invalid token" });
+  }
 }
